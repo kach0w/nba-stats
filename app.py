27bda416a375ef21ada885d4e481bf9ffbd823e2
Kachow <karsab343@gmail.com>
2023-12-31 16:46:28 -0800

    asdf

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ fig.update_layout(xaxis_title='Date', yaxis_title='Margin')
 fig.update_traces(marker=dict(size=15))
 
 st.header("🏀 {}".format(us))
-st.write("### All Games - (" + df['win'].value_counts(0) + " wins " + df['win'].value_counts(1) + " losses)")
+st.write("### All Games - (" + len(df[df['win'] == "green"]) + " wins " + len(df[df['win'] == "red"]) + " losses)")
 st.plotly_chart(fig)
 
 current_date = datetime.now().strftime("%Y-%m-%d")
